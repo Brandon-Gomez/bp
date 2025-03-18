fe17151b0ee22c1d86b95201186cc3aa3a17cee8
Brandon-Gomez <salinasgomez1232@gmail.com>
2025-03-18 01:29:04 -0500

    splash activity view

--- app/src/main/java/com/example/bookpedia/SplashActivity.kt
+++ app/src/main/java/com/example/bookpedia/SplashActivity.kt
@@ -1,33 +1,57 @@
 package com.example.bookpedia
 
+import android.R.attr.text
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            //IntroScreen(onGetClick = {})
+        }
+
     }
 
 }
@@ -55,25 +79,91 @@ fun HeaderSection() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .height(650.dp)
+            .height(850.dp)
     ) {
         Image (
             painter = painterResource(id = R.drawable.bg1),
-            contentDescription = null,
             contentScale = ContentScale.Crop,
+            contentDescription = null,
             modifier = Modifier.matchParentSize()
         )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(
+                    brush = androidx.compose.ui.graphics.Brush.verticalGradient(
+                        colors = listOf(
+                            colorResource(R.color.transparent),
+                            colorResource(R.color.black)
+                        )
+                    )
+                )
+        )
+
         Column (
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.matchParentSize()
+            modifier = Modifier.width(300.dp).align(Alignment.Center)
         ){
             Image(
-                painter = painterResource(id = R.drawable.woman),
+                painter = painterResource(id = R.drawable.logo),
                 contentDescription = null,
-                modifier = Modifier.size(360.dp)
+                modifier = Modifier.size(260.dp)
 
             )
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Explore, read and share your favorite books.",
+                color = colorResource(R.color.white),
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                ),
+
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = { /* Acción del botón */ },
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.brown)),
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "GET IN",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+
         }
+
+        }
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
